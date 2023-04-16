7b9a3b58808d2bbcddd6644cc15923fa92b82a05
perezlathaniel <perezlathaniel@outlook.com>
2023-04-16 17:45:20 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadKey();
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName = Console.ReadLine();
+
+            Console.Write("Type your last name: ");
+            string myLastName = Console.ReadLine();
+
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName + "!");
+            Console.ReadKey();
+
+        }
+    }
+}
